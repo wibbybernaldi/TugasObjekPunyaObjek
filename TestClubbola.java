8d9c68b08c29b3a2f91c35b397231b9db418b950
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-06-10 14:02:35 +0700

    Tugas Objek Dalam Objek
    Perubahan Dikit

--- TestClubbola.java
+++ TestClubbola.java
@@ -5,19 +5,19 @@ package com.company.Clubbola;
  */
 public class TestClubbola {
     public static void main(String[] args) {
-        Pemain pemain1 = new Pemain("Steven Gerrard", "30 Mei 1980", "Inggris", "Whiston, Merseyside");
+        Pemain pemainpertama = new Pemain("Steven Gerrard", "30 Mei 1980", "Inggris", "Whiston, Merseyside");
 
 
-        Clubbola club1 = new Clubbola(pemain1, "Liver Pool", "Merah");
-        System.out.println(club1.toString());
+        Clubbola clubpertama = new Clubbola(pemainpertama, "Liver Pool", "Merah");
+        System.out.println(clubpertama.toString());
 
-        club1.setWarnaJersie("Hitam");
+        clubpertama.setWarnaJersie("Hitam");
 
-        pemain1.setNamaPemain("Dirk Kuyt");
-        pemain1.setTglLahir("22 Juli 1980");
-        pemain1.setTmpLahir("Katwijk aan Zee");
-        pemain1.setKewarganegaraan("Belanda");
+        pemainpertama.setNamaPemain("Dirk Kuyt");
+        pemainpertama.setTglLahir("22 Juli 1980");
+        pemainpertama.setTmpLahir("Katwijk aan Zee");
+        pemainpertama.setKewarganegaraan("Belanda");
 
-        System.out.println(club1.toString());
+        System.out.println(clubpertama.toString());
     }
 }
